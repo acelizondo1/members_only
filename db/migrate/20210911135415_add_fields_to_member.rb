546f0d6999fc8005f98912cb36bc5a6fdc1f4823
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-11 09:30:23 -0500

    Finished member model with new fields and connection with Posts

---
+++ db/migrate/20210911135415_add_fields_to_member.rb
@@ -0,0 +1,6 @@
+class AddFieldsToMember < ActiveRecord::Migration[6.1]
+  def change
+    add_column :members, :first_name, :string
+    add_column :members, :last_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_230303) do
+ActiveRecord::Schema.define(version: 2021_09_11_142700) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_09_10_230303) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
@@ -29,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_09_10_230303) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "member_id"
   end
 
 end
