563f4d923a0f10dea76748ed3691051406ea6729
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-12 09:09:06 -0500

    changing variable type for notification tmeout

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-    const notice = document.querySelectorAll(".notification");
+    let notice = document.querySelectorAll(".notification");
     if (notice.length > 0){
         setTimeout(function(){
             notice[0].remove();
